42c1b89df98e3c51b086c74ce6575361bdd4f8cb
lolflobby-eng <lolflobby@gmail.com>
2025-12-15 16:01:56 +0000

    Update

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -50,16 +50,16 @@ export async function updateSession(request: NextRequest) {
         data: { user },
     } = await supabase.auth.getUser()
 
-    if (
-        !user &&
-        !request.nextUrl.pathname.startsWith('/auth') &&
-        request.nextUrl.pathname !== '/auth'
-    ) {
-        // no user, potentially respond by redirecting the user to the login page
-        const url = request.nextUrl.clone()
-        url.pathname = '/auth'
-        return NextResponse.redirect(url)
-    }
+    // if (
+    //     !user &&
+    //     !request.nextUrl.pathname.startsWith('/auth') &&
+    //     request.nextUrl.pathname !== '/auth'
+    // ) {
+    //     // no user, potentially respond by redirecting the user to the login page
+    //     const url = request.nextUrl.clone()
+    //     url.pathname = '/auth'
+    //     return NextResponse.redirect(url)
+    // }
 
     return supabaseResponse
 }
