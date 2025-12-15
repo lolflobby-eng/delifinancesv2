603db86c883ad751e6b441cb5133a69f136d5e1f
lolflobby-eng <lolflobby@gmail.com>
2025-12-15 15:43:18 +0000

    Update layout.tsx

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -17,7 +17,7 @@ export default async function ProtectedLayout({
                 getAll() {
                     return cookieStore.getAll()
                 },
-                setAll(cookiesToSet) {
+                setAll(cookiesToSet: { name: string, value: string, options: any }[]) {
                     // Layouts strictly can't set cookies in Next.js Server Components,
                     // but we need the client for reading.
                 },
